2ff4f00bfdb378bf0f2662017fe6dfdeac712f31
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-03-23 14:58:29 +0900

    Line feed code recognition.

--- addPokemon.js
+++ addPokemon.js
@@ -19,28 +19,18 @@ controller.spawn({
 }).startRTM();
 
 controller.hears(["(help)"], ['direct_message'], (bot,message) =>{
-	bot.reply(message,"戦闘に使用するポケモンのNNを入力することで，ポケモンのデータが表示されます．");
+	bot.reply(message,"レベル\nNN\nトレーナー名\n持ち物\n技1\n技2\n技3\n技4\n努力値");
 });
 
 controller.hears(["(.*)"], [ 'direct_message' ], (bot, message) => {
-	var name = message.match[1];
-	var trainer_id = "";
-	var trainer_name = "";
-	var searchSql = 'select * from pokemon where name = ?';
-	con.query(searchSql,[name], function(err, rows, fields){
-		if(err) {
-			 console.log('err: ' + err); 
-		}
-		trainer_id = rows[0].trainer_id;
-		var trainerSql = 'select * from trainer where ID = ?';
-		con.query(trainerSql,[trainer_id], function(err,result,fields){
-			if(err) {
-				console.log('err: ' + err);
-			}
-			trainer_name = result[0].name;
-;
-			bot.reply(message, 'レベル : ' + rows[0].level + '\n トレーナー名 : ' + trainer_name + '\n 持ち物 : ' + rows[0].item + '\n 技1 : ' + rows[0].move1 + '\n 技2 : ' + rows[0].move2 + '\n 技3 : ' + rows[0].move3 + '\n 技4 : ' + rows[0].move4 + '\n 努力値 : ' + rows[0].effort_value);
-			//test
-		});
-	});
+	var level = message.text.split("\n")[0];
+	var name = message.text.split("\n")[1];
+	var trainer = message.text.split("\n")[2];
+	var item = message.text.split("\n")[3];
+	var move1 = message.text.split("\n")[4];
+	var move2 = message.text.split("\n")[5];
+	var move3 = message.text.split("\n")[6];
+	var move4 = message.text.split("\n")[7];
+	var effort_value = message.text.split("\n")[8];
+
 });

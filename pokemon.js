ac513e7446b1415c9ddbbbe563acee542614aa37
Sho Tanaka <canaryyellow.nl@gmail.com>
2018-03-25 23:40:19 +0900

    Merged addTrainer.js and addPokemon.js.

--- pokemon.js
+++ pokemon.js
@@ -19,11 +19,46 @@ controller.spawn({
 }).startRTM();
 
 controller.hears(["(help)"], ['direct_message'], (bot,message) =>{
-	bot.reply(message,"戦闘に使用するポケモンのNNを入力することで，ポケモンのデータが表示されます．");
+	bot.reply(message,">ポケモン追加\nレベル\nNN\nトレーナー名\n持ち物\n技1\n技2\n技3\n技4\n努力値\n>トレーナー追加\nトレーナー名\nトレーナーID\n>ポケモン確認\nNN");
 });
 
-controller.hears(["(.*)"], [ 'direct_message' ], (bot, message) => {
-	var name = message.match[1];
+controller.hears(["(ポケモン追加)"], [ 'direct_message' ], (bot, message) => {
+	var level = message.text.split("\n")[1];
+	var nickname = message.text.split("\n")[2];
+	var trainer = message.text.split("\n")[3];
+	var item = message.text.split("\n")[4];
+	var move1 = message.text.split("\n")[5];
+	var move2 = message.text.split("\n")[6];
+	var move3 = message.text.split("\n")[7];
+	var move4 = message.text.split("\n")[8];
+	var effort_value = message.text.split("\n")[9];
+	var trainer_id;
+	
+	var searchTrainerSql = "select * from trainer where name = ?";
+	con.query(searchTrainerSql, [trainer], function(err,result,fields){
+		if(err) console.log('err: ' + err);
+		trainer_id = result[0].ID;
+		
+		var insertSql = "insert into pokemon(level,name,trainer_id,item,move1,move2,move3,move4,effort_value) values (?,?,?,?,?,?,?,?,?)";
+		con.query(insertSql,[level,nickname,trainer_id,item,move1,move2,move3,move4,effort_value], function(err,res){
+			bot.reply(message,"登録しました！");
+		});
+	});
+
+});
+
+controller.hears(["(トレーナー追加)"], [ 'direct_message' ], (bot, message) => {
+	var name = message.text.split("\n")[1];
+	var trainer_id = message.text.split("\n")[2];
+	var insertSql = "insert into trainer(name,trainer_id) values (?,?)";
+	con.query(insertSql,[name,trainer_id], function(err,res){
+		bot.reply(message,"登録しました！");
+	});
+
+});
+
+controller.hears(["(ポケモン確認)"], [ 'direct_message' ], (bot, message) => {
+	var name = message.text.split("\n")[1];
 	var trainer_id = "";
 	var trainer_name = "";
 	var searchSql = 'select * from pokemon where name = ?';
